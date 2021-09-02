d9391fd77142676ac1f56de0af2886d34b128450
Procech20 <mupinoe@gmail.com>
2021-09-02 04:02:10 -0700

    Added header file for the project

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,35 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * _isupper - checks for uppercase characters
+ * _isdigit - checks for a digit(0 throughj 9)
+ * mul - multiplies two integers
+ * print_numbers - prints the numbers, from 0 to 9, followed by new line
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by new line
+ * more_numbers - prints 10 times the  nmbers, from 0 to 14,
+ * followed by new line
+ * print_line - draws a straight line in the terminal
+ * print_diagonal - draws a diagonal line in the terminal
+ * print_square - prints a square, followed by a new line
+ * fizz_buzz - prints the numbers from 1 to 100, followed by new line
+ * prints "Fizz" for multiples of three prints, instead of the number
+ * prints "Buzz" for the multiples of five prints
+ * print_triangle - draws a triangle in the terminal, followed by new line
+ *
+ **/
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+
