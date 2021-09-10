09d780af55f9dfbabee9a68c46849f94627c17e5
Procech20 <mupinoe@gmail.com>
2021-09-10 08:08:45 -0700

    Added some fixes

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,10 @@
+/**
+ * print_buffer - Prints the content of size bytes of the buffer poin ted by b
+ * @b - points to a buffer
+ * @size - size bytes of the outputed content
+ * Return: 10 bytes per line
+ */
+void print_buffer(char *b, int size)
+{
+	printf('\n')
+}
