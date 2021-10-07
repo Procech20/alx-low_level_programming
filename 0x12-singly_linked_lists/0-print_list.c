91d7801ab5cef9ca60193626b71f82e29ac9c2e8
Procech20 <mupinoe@gmail.com>
2021-10-07 14:07:42 -0700

    Create working files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,19 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
 
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: pointer to struct.
- *
- * Return:  the number of nodes.
- */
+ * print_list - print all elements of a linked list
+ * @h: pointer to linked list
+ * Return: number of nodes in linked list
+ **/
 size_t print_list(const list_t *h)
 {
-int count = 0;
+	if (h == NULL)
+		return (0);
+	printf("[%u] %s\n", h->len, h->str);
 
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-count++;
-}
-return (count);
+	if (h->next != NULL)
+		return (print_list(h->next) + 1);
+	return (1);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,44 +1,56 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
 #include <string.h>
-/**
- * _strlen - returns the length of a string.
- * @s: character value.
- *
- *Return: i value
- */
-int _strlen(const char *s)
-{
-int i;
 
-for (i = 0; s[i] != 0; i++)
-;
-return (i);
-}
 /**
- * add_node_end -  adds a new node at the end of a list_t list.
- * @head:pointer so struct.
- * @str: string pointer.
- *
- * Return: the address of the new element.
- */
+ * _strnlen - returns the length of a string
+ * @s: string to count
+ * Return: lenth of string
+ **/
+int _strnlen(const char *s)
+{
+	int i;
+
+	if (s == NULL)
+		return (0);
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node_end - add a new node at end of a linked list
+ * @head: address of beginning of string
+ * @str: string to add to new node
+ * Return: address of new element, NULL if failed
+ **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new = (list_t *) malloc(sizeof(list_t));
-list_t *end = *head;
+	list_t *new, *next;
 
-if (new == NULL)
-return (NULL);
-new->str = strdup(str);
-new->len = _strlen(str);
-new->next = NULL;
-if (*head == NULL)
-(*head) = new;
-else
-{
-while (end->next != NULL)
-end = end->next;
-}
-if (end)
-end->next = new;
-return (new);
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = _strnlen(str);
+	new->next = NULL;
+
+	next = *head;
+
+	if (next == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (next->next != NULL)
+		next = next->next;
+
+	next->next = new;
+	return (new);
 }
